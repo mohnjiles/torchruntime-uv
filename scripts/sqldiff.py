858bd12c4b78e78fcfd016c2464eb4d4d93eca4a
cmdr2 <secondary.cmdr2@gmail.com>
2025-02-13 14:47:56 +0530

    sqldiff program to include the changes in PCI DB update releases

---
+++ scripts/sqldiff.py
@@ -0,0 +1,34 @@
+import sys
+import sqlite3
+
+if len(sys.argv) < 3:
+    print("Usage: python sqldiff.py <file1> <file2>")
+    exit()
+
+QUERY = "SELECT * FROM pci_ids"
+COL_NAME_QUERY = "PRAGMA table_info(pci_ids)"
+
+db1 = sys.argv[1]
+db2 = sys.argv[2]
+
+conn1 = sqlite3.connect(db1)
+conn2 = sqlite3.connect(db2)
+
+cursor1 = conn1.cursor()
+cursor2 = conn2.cursor()
+
+res1 = set(cursor1.execute(QUERY).fetchall())
+res2 = set(cursor2.execute(QUERY).fetchall())
+
+cols = cursor1.execute(COL_NAME_QUERY).fetchall()
+cols = tuple(col[1] for col in cols)
+
+print("--- ADDITIONS ---")
+print(cols)
+for row in res2 - res1:
+    print(row)
+
+print("--- DELETIONS ---")
+print(cols)
+for row in res1 - res2:
+    print(row)
